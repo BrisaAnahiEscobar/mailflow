abda8b7c24d080b17d2ec6d6057aa78c7cd288fc
BrisaAnahiEscobar <brisaescobar13@gmail.com>
2026-06-25 00:29:30 -0300

    modelo de email y lecura TTS dinamica

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(const MailFlowApp());
@@ -17,49 +17,6 @@ class MailFlowApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final FlutterTts tts = FlutterTts();
-
-Future<void> leerCorreo() async {
- /*await tts.setLanguage("es-ES");
-  await tts.setSpeechRate(0.45);
-  await tts.setPitch(1.0);
-  await tts.setVolume(1.0);
-*/
-  await tts.speak(
-    "Hola Brisa. Tienes un correo nuevo."
-  );
-}
-  print("Botón presionado");
-
-  var resultadoIdioma = await tts.setLanguage("es-ES");
-  print("Idioma: $resultadoIdioma");
-
-  var resultado = await tts.speak(
-    "Hola Brisa. Tienes un correo nuevo.",
-  );
-
-  print("Speak: $resultado");
-}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("MailFlow"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: leerCorreo,
-          child: const Text("Leer correo"),
-        ),
-      ),
     );
   }
 }
---
+++ frontend/lib/models/email.dart
@@ -0,0 +1,9 @@
+class Email {
+  final String remitente;
+  final String asunto;
+
+  Email({
+    required this.remitente,
+    required this.asunto,
+  });
+}
