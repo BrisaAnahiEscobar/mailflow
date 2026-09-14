986172849e4ec7f8d6e45e5c0a798264d99e0465
BrisaAnahiEscobar <brisaescobar13@gmail.com>
2026-09-14 15:06:51 -0300

    agrego conexiones multiprovider

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+
+import 'providers/auth_provider.dart';
+import 'providers/gmail_provider.dart';
 import 'providers/tts_provider.dart';
 import 'screens/home_page.dart';
+import 'screens/login_page.dart';
+import 'services/auth_service.dart';
+import 'services/gmail_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   final ttsProvider = TtsProvider();
   await ttsProvider.inicializar();
 
+  final authProvider = AuthProvider(AuthService());
+  await authProvider.inicializar(clientId: dotenv.env['GOOGLE_CLIENT_ID']!);
+
+  final gmailProvider = GmailProvider(GmailService());
+
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => ttsProvider,
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: ttsProvider),
+        ChangeNotifierProvider.value(value: authProvider),
+        ChangeNotifierProvider.value(value: gmailProvider),
+      ],
       child: const MailFlowApp(),
     ),
   );
@@ -26,7 +43,12 @@ class MailFlowApp extends StatelessWidget {
       title: 'MailFlow',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      home: HomePage(),
+      home: Consumer<AuthProvider>(
+        builder: (context, auth, _) {
+          final sesionCompleta = auth.estaAutenticado && auth.tieneAccesoGmail;
+          return sesionCompleta ? HomePage() : const LoginPage();
+        },
+      ),
     );
   }
 }
